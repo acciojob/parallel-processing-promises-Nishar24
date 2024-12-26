9b5a11932410319ddfe84bf4f0ad9c838cae17f9
Nishar24 <mdnishar75640@gmail.com>
2024-12-26 23:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+const imgUrls = [
+  'https://via.placeholder.com/150',
+  'https://via.placeholder.com/200',
+  'https://via.placeholder.com/250',
 ];
+
+const downloadImagesButton = document.getElementById('download-images-button');
+const outputDiv = document.getElementById('output');
+
+function downloadImages(urls) {
+  const imagePromises = urls.map((url) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = url;
+
+      img.onload = () => {
+        resolve(img);
+      };
+
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${url}`);
+      };
+    });
+  });
+
+  return Promise.all(imagePromises);
+}
+
+downloadImagesButton.addEventListener('click', async () => {
+  try {
+    const images = await downloadImages(imgUrls);
+    images.forEach(img => {
+      outputDiv.appendChild(img);
+    });
+  } catch (error) {
+    console.error(error);
+    outputDiv.textContent = error;
+  }
+});
